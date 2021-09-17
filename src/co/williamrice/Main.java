edfac3d6c0c02579d3b8a72d00feff950923b04b
William Rice <william.rice192@gmail.com>
2021-09-16 20:36:44 -0400

    refactored code to abstract truck names
    moved names of trucks to variables. Still needs documentation

--- src/co/williamrice/Main.java
+++ src/co/williamrice/Main.java
@@ -1,6 +1,6 @@
 package co.williamrice;
 
-import java.time.DayOfWeek;
+
 import java.util.Scanner;
 
 class Main{
@@ -8,13 +8,36 @@ class Main{
     {
         final String NAME_OF_PARK = "Keeneland";
 
-        Scanner scanner = new Scanner(System.in);
+        String sundayTruck1 = "Food Paradise";
+        String sundayTruck2 = "The Cake Shack";
 
+        String mondayTruck1 = "The Extreme Burger";
+        String mondayTruck2 = "The Grill Queen";
+
+        String tuesdayTruck1 = "Eat Fest";
+        String tuesdayTruck2 = "Frank's Hot Dogs";
+        
+        String wednesdayTruck1 = "The Urban Burger";
+        String wednesdayTruck2 = "Mexican Mobile";
+
+        String thursdayTruck1 = "Sweet Eats";
+        String thursdayTruck2 = "The Mobile Bakery";
+
+        String fridayTruck1 = "Mr. Burger";
+        String fridayTruck2 = "The Rolling Pizzeria";
+
+        String saturdayTruck1 = "Harry's Burger Bus";
+        String saturdayTruck2 = "Munch Mobile";
+
+        String triviaTheme = "U.S Presidents";
+        String karaokeTheme = "80's Rock N Roll";
+
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to " + NAME_OF_PARK);
         System.out.println("\n\tWe are delighted you are interested in the food trucks coming to our park and our special events.");
         System.out.println("\nPlease enter a day of the week (1 for Sunday, 2 for Monday, … ,7 for Saturday): ");
-        short dayNumber = scanner.nextShort();
+        Integer dayNumber = scanner.nextInt();
 
         if (dayNumber >= 1 && dayNumber <= 7){
             System.out.println("\nFood trucks and events for the remainder of the week: ");
@@ -22,38 +45,40 @@ class Main{
             switch (dayNumber) {
                 case 1:
                     System.out.println("\n\tSunday Night:");
-                    System.out.println("\t\tFood Paradise");
-                    System.out.println("\t\tThe Cake Shack");
+                    System.out.println("\t\t" + sundayTruck1);
+                    System.out.println("\t\t" + sundayTruck2);
                     dayNumber++;
                 case 2:
                     System.out.println("\n\tMonday Night:");
-                    System.out.println("\t\tThe Extreme Burger");
-                    System.out.println("\t\tThe Grill Queen");
+                    System.out.println("\t\t" + mondayTruck1);
+                    System.out.println("\t\t" + mondayTruck2);
                     dayNumber++;
                 case 3:
                     System.out.println("\n\tTuesday Night:");
-                    System.out.println("\t\tEat Fest");
-                    System.out.println("\t\tFrank's Hot Dogs");
+                    System.out.println("\t\t" + tuesdayTruck1);
+                    System.out.println("\t\t" + tuesdayTruck2);
+                    System.out.println("\t\tSPECIAL EVENT: Trivia Night - " + triviaTheme);
                     dayNumber++;
                 case 4:
                     System.out.println("\n\tWednesday Night:");
-                    System.out.println("\t\tThe Urban Burger");
-                    System.out.println("\t\tMexican Mobile");
+                    System.out.println("\t\t" + wednesdayTruck1);
+                    System.out.println("\t\t" + wednesdayTruck2);
                     dayNumber++;
                 case 5:
-                    System.out.println("\n\tThurday Night:");
-                    System.out.println("\t\tSweet Eats");
-                    System.out.println("\t\tThe Mobile Bakery");
+                    System.out.println("\n\tThursday Night:");
+                    System.out.println("\t\t" + thursdayTruck1);
+                    System.out.println("\t\t" + thursdayTruck2);
                     dayNumber++;
                 case 6:
                     System.out.println("\n\tFriday Night:");
-                    System.out.println("\t\tMr. Burger");
-                    System.out.println("\t\tThe Rolling Pizzeria");
+                    System.out.println("\t\t" + fridayTruck1);
+                    System.out.println("\t\t" + fridayTruck2);
+                    System.out.println("\t\tSPECIAL EVENT: Karaoke Night - " + karaokeTheme);
                     dayNumber++;
                 case 7:
                     System.out.println("\n\tSaturday Night:");
-                    System.out.println("\t\tHarry's Burger Bus");
-                    System.out.println("\t\tMunch Mobile");
+                    System.out.println("\t\t" + saturdayTruck1);
+                    System.out.println("\t\t" + saturdayTruck2);
             }
         }
         else{
